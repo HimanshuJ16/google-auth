395df5c0737d25566168fd0ab30475725359a685
Himanshu Jangir <hjuberoi16@gmail.com>
2024-01-05 23:35:04 +0530

    first commit

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const GOOGLE_CLIENT_SECRET = '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';
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    // callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://google-auth-demo-vvap.onrender.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
