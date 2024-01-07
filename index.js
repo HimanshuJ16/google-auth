cbdd7d6376e4c72d458eb170ec947533049f2906
Himanshu Jangir <hjuberoi16@gmail.com>
2024-01-07 14:26:46 +0530

    first commit

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://google-auth-demo-vvap.onrender.com/auth/google/callback"
   },
   async function (accessToken, refreshToken, profile, done) {
     userProfile=profile;
